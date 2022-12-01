c0a2d243573c99c1ca9357877bf24cf8f222d181
GurkNathe <ethan.c.krug@gmail.com>
2022-12-01 13:54:14 -0800

    Adding random walk algorithm.

--- main/algorithms/RandomWalk.py
+++ main/algorithms/RandomWalk.py
@@ -1,2 +1,39 @@
+import pygame
+import random
+import time
+
+
+def reconstruct_path(came_from, current, draw):
+    for current in reversed(came_from):
+        if not current.is_start():
+            current.make_path()
+            draw()
+
+
 def rand_walk(draw, grid, start, end):
-    pass
+    came_from = []
+    current = start
+
+    while current != end:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        neighbor = random.randint(0, len(current.neighbors) - 1)
+        came_from.append(current)
+        current = current.neighbors[neighbor]
+
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+
+        if current != start:
+            current.uncheck()
+
+        draw()
+
+        if current != start:
+            current.check()
+
+    return False
