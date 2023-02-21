08bc59f851432b8eef4d7169bba0d9369d59d56f
GurkNathe <ethan.c.krug@gmail.com>
2023-02-20 16:50:32 -0800

    Changing upload script.

--- upload.sh
+++ upload.sh
@@ -8,17 +8,25 @@ fi
 
 # Enable extended globbing.
 shopt -s extglob 
-dir="main/testing/results"
 
+dir = "main/testing/results"
+
+# Check if directory exists
 if [ -d "$dir" ]; then
-    file_count=$(ls -1 "$dir" | wc -l)
+    # List and count every file in the directory
+    file_count = $(ls -1 "$dir" | wc -l)
+
+    # Check if there are more than one file in the directory
     if [ "$file_count" -gt 1 ]; then
 
         # Navigate to the subfolder containing the CSV files.
         cd "$dir"
         rm !(Generated_Maze-Example.csv)
+
         echo "Deleting CSV files from ./main/testing/results"
     fi
+else
+    echo "Directory does not exist."
 fi
 
 # Upload to repository
