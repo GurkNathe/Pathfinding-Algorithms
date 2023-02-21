651d82dd69f355e52fa54463501c2006523f4a7c
GurkNathe <ethan.c.krug@gmail.com>
2023-02-20 16:09:55 -0800

    Adding testing csv file deletion, upon upload.

--- upload.sh
+++ upload.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 if [ -z "$1" ]; then
     echo "Error: Please provide a commit message."
     exit 1
 fi
+
+shopt -s extglob # enable extended globbing
+
+cd main/testing/results  # navigate to the subfolder containing the CSV files
+rm !(Generated_Maze-Example.csv)
 
 git add -A
 git commit -m "$1"
